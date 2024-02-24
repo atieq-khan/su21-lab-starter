869093a8e22afa2613e9c3fc51dd1ae32044a3a0
atieq-khan <atieqkhan12@gmail.com>
2024-02-25 01:54:12 +0500

    The complete lab01 with all four exerises and with all four ELF files. Done on 24-02-2024

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -5,15 +5,39 @@
 There are two different ways to iterate through a string.
 1st way hint: strlen() may be useful
 2nd way hint: all strings end in a null terminator */
-int num_occurrences(char *str, char letter) {
-    /* TODO: implement num_occurances */
-    return 0;
+int num_occurrences(char *str, char letter) 
+{
+   int count = 0;
+   int length = strlen(str);
+   for (int i = 0; i < length; i++) 
+   {
+        if (str[i] == letter) 
+        {
+	    count++;
+	}
+    }
+    return count;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
 Each sequence will end with a NULL terminator and will have up to 20 nucleotides.
 All letters will be upper case. */
+
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
-    /* TODO: implement compute_nucleotide_occurances */
+        
+    for (char nucleotide = 'A'; nucleotide <= 'T'; nucleotide++) 
+    {
+        int nucleotide_count = num_occurrences(dna_seq->sequence, nucleotide);
+
+        if (nucleotide == 'A') {
+            dna_seq->A_count = nucleotide_count;
+        } else if (nucleotide == 'C') {
+            dna_seq->C_count = nucleotide_count;
+        } else if (nucleotide == 'G') {
+            dna_seq->G_count = nucleotide_count;
+        } else if (nucleotide == 'T') {
+            dna_seq->T_count = nucleotide_count;
+        }
+    }
     return;
 }
