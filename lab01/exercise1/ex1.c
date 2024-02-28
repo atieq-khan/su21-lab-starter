b561196064baef41f0d705889632531733dc8297
Atieq Ur Rehman <73298311+atieq-khan@users.noreply.github.com>
2024-02-28 22:04:08 +0500

    fixing a small mistake ex1.c
    I accidentally committed the wrong exe1.c file

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -24,20 +24,8 @@ Each sequence will end with a NULL terminator and will have up to 20 nucleotides
 All letters will be upper case. */
 
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
-        
-    for (char nucleotide = 'A'; nucleotide <= 'T'; nucleotide++) 
-    {
-        int nucleotide_count = num_occurrences(dna_seq->sequence, nucleotide);
-
-        if (nucleotide == 'A') {
-            dna_seq->A_count = nucleotide_count;
-        } else if (nucleotide == 'C') {
-            dna_seq->C_count = nucleotide_count;
-        } else if (nucleotide == 'G') {
-            dna_seq->G_count = nucleotide_count;
-        } else if (nucleotide == 'T') {
-            dna_seq->T_count = nucleotide_count;
-        }
-    }
-    return;
+     dna_seq->A_count = num_occurrences(dna_seq->sequence, 'A');
+     dna_seq->C_count = num_occurrences(dna_seq->sequence, 'C');
+     dna_seq->G_count = num_occurrences(dna_seq->sequence, 'G');
+     dna_seq->T_count = num_occurrences(dna_seq->sequence, 'T');
 }
